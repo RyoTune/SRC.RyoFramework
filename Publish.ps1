b40a796693266008484fbf3d5ae09404d3b94337
RyoTune <zutomayofan50@gmail.com>
2025-08-28 23:16:16 -0500

    Fix workflow.

--- Publish.ps1
+++ Publish.ps1
@@ -339,7 +339,7 @@ function Publish-Common {
         $PublishTarget=""
     )
 
-    ls -d
+    Get-ChildItem -Directory
     Remove-Item $Directory -Recurse -ErrorAction SilentlyContinue
     New-Item $Directory -ItemType Directory -ErrorAction SilentlyContinue
 	$arguments = "$(Get-Common-Publish-Args -AllowDeltas $AllowDeltas) --outputfolder `"$Directory`" --publishtarget $PublishTarget"
