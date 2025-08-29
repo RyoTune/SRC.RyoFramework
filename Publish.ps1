f0b294eaf29c0c6ae4b269ebb149eeba382eaf12
RyoTune <zutomayofan50@gmail.com>
2025-08-28 23:04:06 -0500

    Fix workflow.

--- Publish.ps1
+++ Publish.ps1
@@ -191,7 +191,7 @@ param (
 )
 
 ## => User: Publish Output
-$publishBuildDirectory = "."      # Build directory for current version of the mod.
+$publishBuildDirectory = "Reloaded"      # Build directory for current version of the mod.
 $deltaDirectory = "Publish/Builds/LastVersion"                # Path to last version of the mod.
 
 $PublishGenericDirectory = "$PublishOutputDir/Generic"        # Publish files for any target not listed below.
