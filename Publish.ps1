7403a589c7400663035563eae0f7336d6c90fc77
RyoTune <zutomayofan50@gmail.com>
2025-08-28 22:41:51 -0500

    Fix workflow.

--- Publish.ps1
+++ Publish.ps1
@@ -164,7 +164,7 @@ param (
     ## => User Config <= ## 
     $ProjectPath = "",
     $PackageName = "SRC.RyoFramework",
-    $PublishOutputDir = "../Publish/ToUpload",
+    $PublishOutputDir = "./Publish/ToUpload",
 
     ## => User: Delta Config
     # Pick one and configure settings below.
@@ -191,7 +191,7 @@ param (
 )
 
 ## => User: Publish Output
-$publishBuildDirectory = "."      # Build directory for current version of the mod.
+$publishBuildDirectory = "./Reloaded"      # Build directory for current version of the mod.
 $deltaDirectory = "Publish/Builds/LastVersion"                # Path to last version of the mod.
 
 $PublishGenericDirectory = "$PublishOutputDir/Generic"        # Publish files for any target not listed below.
